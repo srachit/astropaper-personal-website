90ae6f30c91bc5f3d1212d523dff91e90eac23f3
Rachit Sengupta <rachit_sengupta@intuit.com>
2024-10-01 22:50:26 -0700

    remove cloudflare import

--- astro.config.ts
+++ astro.config.ts
@@ -6,8 +6,6 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
